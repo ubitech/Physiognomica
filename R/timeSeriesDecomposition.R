893508c4758f5636a031e84fababbb80ff80448a
rstudio <rstudio@entropy-analytics>
2018-10-19 08:28:44 +0000

    prepare time series decomposition funtion

--- R/timeSeriesDecomposition.R
+++ R/timeSeriesDecomposition.R
@@ -1,5 +1,17 @@
 timeSeriesDecomposition <- function(prometheus_url,start,end,step,metric){
   print("time series decomposition")
-  
-  
+  mydata1 <- convertPrometheusDataToTabularFormat(prometheus_url,toString(metric1$name),toString(metric1$friendlyName),toString(metric1$dimensions),start,end,step)
+  mydata1
+}
+
+#prometheus_url = "http://212.101.173.70:9090"
+#start = "&start=2018-10-19T07:30:30.781Z"
+#end = "&end=2018-10-19T08:30:10.781Z&step"
+#step = "=1m"
+test_time_series_decomposition <- function() {
+print("execute test_time_series_decomposition")
+metrics_appendix <- read.csv(file="metrics_appendix.csv", header=TRUE, sep=",")
+metric1 <-metrics_appendix[10,]
+
+Physiognomica::timeSeriesDecomposition(prometheus_url,start,end,step,metric1)
 }
